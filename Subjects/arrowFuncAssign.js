855883911298ed250cad361400b7a275a46b7f0d
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-07-17 23:31:14 -0500

    Arrow func assignment

---
+++ Subjects/arrowFuncAssign.js
@@ -0,0 +1,22 @@
+const ten = () => {
+  return 10
+}
+
+const ten2 = _ => {return 10}
+
+
+
+const logger2 = arg => {
+  console.log(arg)
+}
+const logger3 = (arg) => {
+  console.log(arg)
+}
+logger3('hi')
+
+
+
+const add3 = (a, b) => a + b
+
+
+
